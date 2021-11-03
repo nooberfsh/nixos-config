4e400cb09d012ed6d622a22aa0fbce39acfeefdd
nooberfsh <nooberfsh@gmail.com>
2021-11-03 11:10:42 +0800

    export LIBCLANG_PATH

--- packages.nix
+++ packages.nix
@@ -1,54 +1,61 @@
 { config, pkgs, ... }:
 
+let 
+  clang = pkgs.llvmPackages_12.libclang;
+  python3 = pkgs.python39;
+in  
 {
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
     # base
-    gcc
-    gdb
-    git
-    tree
-    unzip
-    vim
-    neovim
-    wget
-    cmake
-    htop
-    tmux
-    python39
-    curl
-    pkg-config
-    gnumake
-    openssl
-    binutils
-    ninja
-    autojump
+    pkgs.gcc
+    pkgs.gdb
+    pkgs.git
+    pkgs.tree
+    pkgs.unzip
+    pkgs.vim
+    pkgs.neovim
+    pkgs.wget
+    pkgs.cmake
+    pkgs.htop
+    pkgs.tmux
+    pkgs.curl
+    pkgs.pkg-config
+    pkgs.gnumake
+    pkgs.openssl
+    pkgs.binutils
+    pkgs.ninja
+    pkgs.autojump
+
+    python3
     clang
-    libclang
 	
     # misc
-    smartmontools
-    cpu-x
+    pkgs.smartmontools
+    pkgs.cpu-x
 
     # desktop
-    libsForQt5.yakuake
+    pkgs.libsForQt5.yakuake
 
     # dev 
-    jetbrains.clion
-    jetbrains.datagrip
-    jetbrains.idea-ultimate
-    dbeaver
-    slack
+    pkgs.jetbrains.clion
+    pkgs.jetbrains.datagrip
+    pkgs.jetbrains.idea-ultimate
+    pkgs.dbeaver
+    pkgs.slack
 
     # language tools
-    stack
-    rustup
-    scala
-    sbt
-    go
-    ruby
-    nodejs
-    jdk8
+    pkgs.stack
+    pkgs.rustup
+    pkgs.scala
+    pkgs.sbt
+    pkgs.go
+    pkgs.ruby
+    pkgs.nodejs
+    pkgs.jdk8
   ];
+
+  # https://github.com/NixOS/nixpkgs/issues/52447
+  environment.variables.LIBCLANG_PATH="${clang.lib}/lib";
 }
